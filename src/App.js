f9bc4d5e7dc48472885b206fc7d2055fa388c705
rmonterodev <rmonterodev@gmail.com>
2021-07-26 17:12:17 -0600

    Final touches

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './components/pages/About';
 import Home from './components/pages/Home';
 import GithubState from './components/context/github/githubState';
 import AlertState from './components/context/alert/alertState';
+import NotFound from './components/pages/NotFound';
 
 import './App.css';
 
@@ -24,6 +25,7 @@ const App = () => {
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About}/>
             <Route exact path='/user/:login' component={User}/>
+            <Route component={NotFound}/>
           </Switch>
         </div>
       </div>
